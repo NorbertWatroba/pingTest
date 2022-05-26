c24d2d30a1142aeb39a45a212348aac2d67ea7a6
NorbertWatroba <norbert@watroba.net>
2022-05-26 13:47:27 +0200

    pick

--- interface.py
+++ interface.py
@@ -1,14 +1,16 @@
 import os
 import re
 import json
+from pick import pick
 
 
 def append_list_validation(string):
-    list = string.split(', ')
+    list = string.split(';')
     output = []
     for e in list:
-        element = e.split('/')
-        output.append({"ip_address": element[0], "name": element[1]})
+        element = e.strip().split('/')
+        if element != ['']:
+            output.append({"ip_address": element[0], "name": element[1]})
     return output
 
 
@@ -18,56 +20,96 @@ def append_single_validation(string):
     return output
 
 
-with open('list.JSON', 'r') as f:
-    json_data = json.load(f)
-    follow_list = [json_dict for json_dict in json_data]
+try:
+    with open('list.JSON', 'r') as f:
+        json_data = json.load(f)
+        follow_list = [json_dict for json_dict in json_data]
+except json.decoder.JSONDecodeError:
+    follow_list = []
 
 operation_list = ['Dodaj', 'Usuń', 'Pokaż listę']
 
-# os.system('cls')
+os.system('clear')
 
-print('Witaj'.center(200, '-'))
+print('Witaj'.center(50, '-'))
 
-for i in range(len(operation_list)):
-    print(f'{i+1} - {operation_list[i]}')
 while True:
-    choice = input('Wybór:\t')
-    match choice:
-        case '1':
+    title = 'Witaj'
+    options = ['Dodaj', 'Usuń', 'Pokaż listę']
+    option, index = pick(options, title)
+    match option:
+        case 'Dodaj':
             print('Podaj adresy do monitorowania:')
             while True:
                 string = input('')
-                if re.match(r'((\d{1,3}\.){1,3}\d{1,3}/.+,\s)+', string):
-                    follow_list.extend(append_list_validation(string))
+                if string == 'q':
+                    os.system('clear')
+                    break
+                if re.match(r'((\d{1,3}\.){1,3}\d{1,3}/.+;\s)+', string):
+                    ex = append_list_validation(string)
+                    for i in ex[::-1]:
+                        for item in follow_list:
+                            if i['name'] == item['name']:
+                                item['ip_address'] = i['ip_address']
+                                ex.remove(i)
+                    follow_list.extend(ex)
                     with open('list.JSON', 'w') as f:
                         f.write(json.dumps(follow_list, indent=2))
+                    os.system('clear')
                     break
                 elif re.match(r'(\d{1,3}\.){1,3}\d{1,3}/.+', string):
-                    follow_list.append(append_single_validation(string))
+                    ex = (append_single_validation(string))
+                    for item in follow_list:
+                        if item['name'] == ex['name']:
+                            item['ip_address'] = ex['ip_address']
+                            os.system('clear')
+                            break
+
                     with open('list.JSON', 'w') as f:
                         f.write(json.dumps(follow_list, indent=2))
+                    os.system('clear')
                     break
                 else:
-                    print('Prawidłowy format to: adres_ip/nazwa, ...')
+                    print('Prawidłowy format to: adres_ip/nazwa; ...')
 
-        case '2':
-            print('Przestań monitorować:')
+        case 'Usuń':
             while True:
-                string = input('')
-                objects = string.split(', ')
-                for o in objects:
-                    for i in follow_list:
-                        if i['name'] == o:
-                            follow_list.remove(i)
-                            with open('list.JSON', 'w') as f:
-                                f.write(json.dumps(follow_list, indent=2))
-                break
+                if follow_list:
+                    for pos, i in enumerate(follow_list, start=1):
+                        print(f'{pos:<3} - {i["ip_address"]:>15} / {i["name"]}')
+                    print('Przestań monitorować:')
+                    string = input('')
+                    if string == 'q':
+                        os.system('clear')
+                        break
+                    objects = string.split('; ')
+                    for o in objects:
+                        present = False
+                        if o == 'all':
+                            follow_list.clear()
+                            os.system('clear')
+                            break
+                        else:
 
-        case '3':
+                            for i in follow_list:
+                                if i['name'] == o:
+                                    follow_list.remove(i)
+                                    present = True
+                                    with open('list.JSON', 'w') as f:
+                                        f.write(json.dumps(follow_list, indent=2))
+                        if not present:
+                            print(f'{o} nie istnieje')
+                    os.system('clear')
+                    break
+                else:
+                    print('Nie monitorujesz żadnych pojazdów')
+                    os.system('clear')
+                    break
+
+        case 'Pokaż listę':
+
             if follow_list:
                 for pos, i in enumerate(follow_list, start=1):
                     print(f'{pos} - {i["ip_address"]:>15} / {i["name"]}')
             else:
                 print('Lista jest pusta')
-        case _:
-            print('Wybierz z listy (1-3)')
--- pinger.py
+++ pinger.py
@@ -14,12 +14,15 @@ def run_io_tasks_in_parallel(tasks):
 def ping_custom(follow_list):
     for item in follow_list:
         ping(item['ip_address'], verbose=True)
+    global stop_threads
+    stop_threads = True
 
 
 def waiter():
     animation_sequence = r"|/-\\"
     idx = 0
     while True:
+        global stop_threads
         print(animation_sequence[idx % len(animation_sequence)], end="\r")
         idx += 1
         time.sleep(0.1)
@@ -27,15 +30,22 @@ def waiter():
         if idx == len(animation_sequence):
             idx = 0
 
+        if stop_threads:
+            break
+
 
 while True:
-    with open('list.JSON', 'r') as f:
-        json_data = json.load(f)
-        follow_list = [json_dict for json_dict in json_data]
-        run_io_tasks_in_parallel([
-            lambda: ping_custom(follow_list),
-            lambda: waiter(),
-        ])
+    stop_threads = False
+    try:
+        with open('list.JSON', 'r') as f:
+            json_data = json.load(f)
+            follow_list = [json_dict for json_dict in json_data]
+    except json.decoder.JSONDecodeError:
+        follow_list = []
+
+    run_io_tasks_in_parallel([
+        lambda: ping_custom(follow_list),
+        lambda: waiter(),
+    ])
 
     time.sleep(7)
-
